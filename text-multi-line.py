dc2d15d171a8f35d7c0a226593648f9bd9151c99
Federico Ruiz Ugalde <memeruiz@gmail.com>
2019-06-11 04:35:00 -0600

    ready! and it reads from stdin also

--- text-multi-line.py
+++ text-multi-line.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 from optparse import OptionParser
-import math, os
+import math, os, sys
 from subprocess import check_output
 from functools import reduce
 
@@ -14,27 +14,30 @@ class Label(object):
     def set_fontsize(self, fontsize):
         self.fontsize=fontsize
         self.each_line_width=int(self.fontsize*0.01*(self.complete_line_width/len(self.text_lines_list)))
+        #print("each line width: ", self.each_line_width)
+        self.rest_width=self.complete_line_width-self.each_line_width*len(self.text_lines_list)
+        #print("Rest width: ", self.rest_width)
 
 
     def get_max_length(self):
         self._text_lines_byte_list=[]
         max_length=0
         for line in self.text_lines_list:
-            print("line: ", line)
+            #print("line: ", line)
             bytes_result=check_output(["textlabel","--font", "\""+str(self.font)+"\"", "--width", str(self.each_line_width), line])
             self._text_lines_byte_list.append(bytes_result.split(b"\n")[:-1])
-            print("bytes list:\n", self._text_lines_byte_list[-1])
-            print("line lenght: ", len(self._text_lines_byte_list[-1]))
+            #print("bytes list:\n", self._text_lines_byte_list[-1])
+            #print("line lenght: ", len(self._text_lines_byte_list[-1]))
             if len(self._text_lines_byte_list[-1]) > max_length:
                 max_length=len(self._text_lines_byte_list[-1])
-        print("Max length: ", max_length)
+        #print("Max length: ", max_length)
         return(max_length)
 
     def fill_lines(self, max_length):
-        print("Filling lines!!! \n \n")
+        #print("Filling lines!!! \n \n")
         for i,line in enumerate(self._text_lines_byte_list):
-            print("line: "+str(i)+"  "+str(line))
-            print("Filling "+str((max_length-len(line))//2)+" zeros")
+            #print("line: "+str(i)+"  "+str(line))
+            #print("Filling "+str((max_length-len(line))//2)+" zeros")
             diff=(max_length-len(line))//2
             more=(max_length-len(line))%2
             for j in range(diff):
@@ -42,16 +45,30 @@ class Label(object):
             for j in range(diff+more):
                 self._text_lines_byte_list[i].append(b'0'*len(line[0]))
 
+    def add_extra_cols(self):
+        self._final_render_bytes=[]
+        extra_margin=self.rest_width//2
+        rest_extra_margin=self.rest_width%2
+        for i, line in enumerate(self._pre_final_render_bytes):
+            self._final_render_bytes.append(b'0'*extra_margin+line+b'0'*(extra_margin+rest_extra_margin))
+
+
+
+
     def get_render(self):
         max_length=self.get_max_length()
         self.fill_lines(max_length)
         for line in self._text_lines_byte_list:
-            print("length of line is: ", len(line))
-            print(line)
-        self._final_render=([reduce(lambda x,y:x+y, i) for i in zip(*self._text_lines_byte_list)])
-        for line in self._final_render:
-            print(line)
-        pass
+            #print("length of line is: ", len(line))
+            #print(line)
+            pass
+        self._text_lines_byte_list.reverse()
+        self._pre_final_render_bytes=([reduce(lambda x,y:x+y, i) for i in zip(*self._text_lines_byte_list)])
+        self.add_extra_cols()
+        final_render=[]
+        for line in self._final_render_bytes:
+            final_render.append(line.decode("utf-8"))
+        return(final_render)
 
 
 
@@ -67,14 +84,17 @@ if __name__=="__main__":
 
     (options, args) = parser.parse_args()
     text=args[0]
+    if text=="-":
+        text=sys.stdin.read()
     text_list=text.split("\n")
 
-    print("text:\n", text)
-    print("text list:\n", text_list)
-    print("Font :", options.font)
-    print("Font size percentage: ", options.fontsize)
+    #print("text:\n", text)
+    #print("text list:\n", text_list)
+    #print("Font :", options.font)
+    #print("Font size percentage: ", options.fontsize)
 
     label=Label(text_list, font=options.font, fontsize=options.fontsize)
-    label.get_render()
-
+    for line in label.get_render():
+        print(line)
+    #print(len(line))
 
